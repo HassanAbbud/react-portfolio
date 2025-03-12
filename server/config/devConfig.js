6605d094dde73434f9a76cb6668ee10926d9da3a
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2025-03-11 23:05:27 -0400

    Re-added devConfig.js

---
+++ server/config/devConfig.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+ 
+// You should name this constant with your mongodb URL 
+const URI = 'mongodb+srv://YOUR_DBNAME';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected successfully');
+    
+    const db = mongoose.connection.db;
+    console.log(`Connected to database: ${db.databaseName}`);
+    
+    // List all collections (this will be empty initially)
+    const collections = await db.listCollections().toArray();
+    console.log('Collections:', collections.map(c => c.name));
+    
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
